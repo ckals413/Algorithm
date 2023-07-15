e50bd0ba9f675c8bb7561d7f071e5a0056e13a13
ckals413 <124526270+ckals413@users.noreply.github.com>
2023-07-15 22:07:32 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int gcd2(int A,int B)
+{
+    if (B == 0)
+        return A;
+    gcd2(B, A%B);
+}
+int main()
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+    int num = gcd2(A, B);
+    printf("%d\n%d", num, A*B / num);
+}
