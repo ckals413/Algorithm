13bc697a3f6bec3ca0ec89887d033c7c1ac52e04
ckals413 <124526270+ckals413@users.noreply.github.com>
2023-07-17 17:49:32 +0900

    [Bronze III] Title: 공 넣기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.c
+++ 백준/Bronze/10810. 공 넣기/공 넣기.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
-
 int main() {
+	int T, n, min, max, c;
+	int arr[100] = { 0, };
+	scanf("%d %d", &T, &n);
 
-  int n, m;
-  int arr[101] = { 0, };
-  int a, b, c;
-
-  scanf("%d %d", &n, &m);
-
-  for(int i = 0; i < m; i++) {
-    scanf("%d %d %d", &a, &b, &c);
-    for(int k = a; k <= b; k++) {
-      arr[k] = c;
-    }
-  }
-
-  for(int i = 1; i <= n; i++) {
-    printf("%d ", arr[i]);
-  }
-
-  return 0;
+	for (int j = 0; j < n; j++) {
+		scanf("%d %d %d", &min, &max, &c);
+		for (int i = min; i <= max; i++) {
+			arr[i] = c;
+		}
+	}
+	
+	for (int i = 1; i <= T; i++) {
+		printf("%d ", arr[i]);
+	}
+	return 0;
 }
