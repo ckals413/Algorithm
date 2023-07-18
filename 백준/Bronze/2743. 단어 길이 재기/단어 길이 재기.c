f1b31502f50da2cffd447a6f9a6ca095947a31be
ckals413 <124526270+ckals413@users.noreply.github.com>
2023-07-19 01:07:19 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+   
+    char str[100] = {" "};
+    int length = 0;
+   
+    scanf("%s", str);
+   
+    length = strlen(str);
+   
+    printf("%d", length);
+}
