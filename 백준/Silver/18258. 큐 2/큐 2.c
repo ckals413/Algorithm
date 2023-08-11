4159dc021b0d68264b1ee11dddc3a944ccec2c15
ckals413 <124526270+ckals413@users.noreply.github.com>
2023-08-12 00:26:41 +0900

    [Silver IV] Title: 큐 2, Time: 684 ms, Memory: 8808 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.c
+++ 백준/Silver/18258. 큐 2/큐 2.c
@@ -1,70 +1,122 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-typedef struct Queue {
-    int data[2000000];
-    int front, rear;
-} Queue;
+// === 원형큐 코드 시작 ===
 
-void init(Queue *q) {
-    q->front = q->rear = -1;
+#define MAX_QUEUE_SIZE 2000000
+typedef int element;
+typedef struct {//큐 타입
+	element data[MAX_QUEUE_SIZE];
+	int front, rear;
+}QueueType;
+
+
+// 큐 초기화 함수
+void init_queue(QueueType* q) {
+	q->front = q->rear = 0;
 }
 
-int is_empty(Queue *q) {
-    return (q->front == q->rear);
+//공백 상태 검출함수
+int is_empty(QueueType* q) {
+	return(q->front == q->rear);//같으면(비어있으면) true
+}
+//포화상태 검출함수
+int is_full(QueueType* q) {
+	return((q->rear + 1) % MAX_QUEUE_SIZE == q->front);
 }
 
-int size(Queue *q) {
-    if (is_empty(q)) {
-        return 0;
-    }
-    return (q->rear - q->front);
+//원형큐 출력함수
+void queue_print(QueueType* q) {
+	printf("QUEUE(front=%d rear=%d)=", q->front, q->rear);
+	if (!is_empty(q)) {
+		int i = q->front;
+		do {
+			i = (i + 1) % (MAX_QUEUE_SIZE);
+			printf("%d |", q->data[i]);
+			if (i == q->rear)
+				break;
+		} while (i != q->front);
+	}
+	printf("\n");
+}
+//삽입함수
+void enqueue(QueueType* q, element item) {
+    if (is_full(q))
+        return -1;
+	q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
+	q->data[q->rear] = item;
 }
 
-void push(Queue *q, int data) {
-    q->data[++(q->rear)] = data;
+//삭제함수
+element dequeue(QueueType* q) {
+    if (is_empty(q))
+        return -1;
+	q->front = (q->front + 1) % MAX_QUEUE_SIZE;
+	return q->data[q->front];
 }
 
-int pop(Queue *q) {
-    if (is_empty(q)) return -1;
-    return q->data[++(q->front)];
+//체크함수
+element peek(QueueType* q) {
+	if (is_empty(q)) {
+        return -1;
+	}
+    return q->data[(q->front + 1) % MAX_QUEUE_SIZE];
 }
 
-int front(Queue *q) {
-    if (is_empty(q)) return -1;
-    return q->data[q->front + 1];
-}
-
-int back(Queue *q) {
-    if (is_empty(q)) return -1;
-    return q->data[q->rear];
-}
+//==== 원형 큐 코드 끝 ====
 
 int main(void) {
-    Queue q;
-    init(&q);
+    QueueType queue;
+    init_queue(&queue);
 
-    int N;
+    int N, a;
+    char ch[6];  // 명령어 문자열을 저장할 배열
     scanf("%d", &N);
 
-    while(N--) {
-        char str[6];
-        scanf("%s", str);
-        if (!strcmp(str, "push")) {
-            int data = 0;
-            scanf("%d", &data);
-            push(&q, data);
+    for (int i = 0; i < N; i++) {
+        scanf("%s", ch);
+
+        if (strcmp(ch, "push") == 0) {  // strcmp 함수를 사용하여 문자열 비교
+            scanf("%d", &a);
+            enqueue(&queue, a);
         }
-        else if (!strcmp(str, "front"))
-            printf("%d\n", front(&q));
-        else if (!strcmp(str, "back"))
-            printf("%d\n", back(&q));
-        else if (!strcmp(str, "pop"))
-            printf("%d\n", pop(&q));
-        else if (!strcmp(str, "size"))
-            printf("%d\n", size(&q));
-        else if (!strcmp(str, "empty"))
-            printf("%d\n", is_empty(&q));
+        else if (strcmp(ch, "pop") == 0) {
+            if (!is_empty(&queue)) {
+                printf("%d\n", dequeue(&queue));
+            }
+            else {
+                printf("-1\n");
+            }
+        }
+        else if (strcmp(ch, "size") == 0) {
+            printf("%d\n", (queue.rear - queue.front + MAX_QUEUE_SIZE) % MAX_QUEUE_SIZE);
+        }
+        else if (strcmp(ch, "empty") == 0) {
+            if (is_empty(&queue)) {
+                printf("1\n");
+            }
+            else {
+                printf("0\n");
+            }
+        }
+        else if (strcmp(ch, "front") == 0) {
+            if (!is_empty(&queue)) {
+                printf("%d\n", queue.data[(queue.front + 1) % MAX_QUEUE_SIZE]);
+            }
+            else {
+                printf("-1\n");
+            }
+        }
+        else if (strcmp(ch, "back") == 0) {
+            if (!is_empty(&queue)) {
+                printf("%d\n", queue.data[queue.rear]);
+            }
+            else {
+                printf("-1\n");
+            }
+        }
     }
+
     return 0;
 }
