267192cce702e173b6812370461c0a434ca4e61b
ckals413 <124526270+ckals413@users.noreply.github.com>
2024-01-24 11:52:35 +0900

    [Bronze V] Title: A+B, Time: 204 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,10 +1,13 @@
-import java.util.Scanner;
+import java.util.*;
+
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        System.out.println(a+b);
-        scanner.close();
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.println(A+B);
+		
+		
+	}
 }
