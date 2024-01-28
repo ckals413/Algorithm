88251d1bc342d97d40e6ccc61e9acf712c397a64
ckals413 <124526270+ckals413@users.noreply.github.com>
2024-01-28 11:01:13 +0900

    [Silver V] Title: K번째 수, Time: 2196 ms, Memory: 377808 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+	public static void main(String [] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		Scanner sc = new Scanner(System.in);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int arr[] = new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		System.out.print(arr[k-1]);
+		
+	}
+}
