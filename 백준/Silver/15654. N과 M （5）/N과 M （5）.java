a4eda1642d3d0257cda8dd7af3298a77121f8971
ckals413 <124526270+ckals413@users.noreply.github.com>
2024-01-30 14:20:41 +0900

    [Silver III] Title: N과 M (5), Time: 428 ms, Memory: 42048 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,56 @@
+///
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static int[] arr; 
+	public static boolean [] visit;
+	public static StringBuilder sb = new StringBuilder();
+	public static int N,M;
+	public static int [] inputArr;
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());//1,2,...N-1,N까지 숫자들중에
+        M = Integer.parseInt(st.nextToken());//자릿수가 M인 모든 경우의 수를 출력하는 문제 
+        inputArr= new int [N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+        	inputArr[i]= Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(inputArr);
+        
+        arr = new int[M]; // M의 자릿수 배열 생성 
+        visit = new boolean[N];
+        dfs(0);
+        
+        System.out.println(sb);
+   
+  }
+    public static void dfs(int depth) {//depth는 깊이 즉 자릿수
+    	if(depth == M) {
+    		for(int val : arr) {
+    			sb.append(val+" ");
+    		}
+    		sb.append("\n");
+    		return;
+    	}
+    	for(int i=0; i<N; i++) {
+    		if(!visit[i]) {
+    			visit[i]=true;
+    			arr[depth]=inputArr[i];
+    			dfs(depth+1);
+    			visit[i]=false;
+    		}
+    	}
+    	
+    }
+}
