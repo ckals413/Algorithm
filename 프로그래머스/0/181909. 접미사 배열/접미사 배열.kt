b5c83256494bc53d9a95d9d986ede6a346ee0f21
ckals413 <124526270+ckals413@users.noreply.github.com>
2025-08-21 23:29:13 +0900

    [level 0] Title: 접미사 배열, Time: 22.05 ms, Memory: 66.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(my_string: String): Array<String> {
+        val suffixes = mutableListOf<String>()
+
+        for (i in my_string.indices) {
+            suffixes.add(my_string.substring(i))
+        }
+        return suffixes.sorted().toTypedArray()
+    }
+}
