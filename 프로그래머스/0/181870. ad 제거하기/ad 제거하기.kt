cdda3ed521fb658c74e57654bd6232b542a889fc
ckals413 <124526270+ckals413@users.noreply.github.com>
2025-09-14 22:54:42 +0900

    [level 0] Title: ad 제거하기, Time: 10.64 ms, Memory: 64.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(strArr: Array<String>): Array<String> {
+        return strArr
+            .filter { !it.contains("ad") }  
+            .toTypedArray()                  
+    }
+}
